00748556ab2f3fc56e4cf1fdba424eca1fdcaf50
Jordan Moore <jordanhmoore@gmail.com>
2022-05-09 15:31:00 -0400

    Some basic styling

--- client/appState/theme.js
+++ client/appState/theme.js
@@ -7,7 +7,6 @@ const ThemeProvider = ({ STYLES, dimensions }) => {
   )
 }
 
-
 const media = {
   mobile: '425px',
   tablet: '768px',
@@ -32,7 +31,12 @@ body {
   height: 100%;
   background-color: rgb(247, 193, 18);
   font-family: 'Open Sans', sans-serif;
-  color: white;
+  color: green;
+  --color1: #00933D;
+  --color2: #00796F;
+  --color3: #00586B;
+  --color4: #2F4858;
+
 }
 
 body {
@@ -49,91 +53,8 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
 }
 
+
+
 `
-
-
-
-
-var calcRange = (baseValues, change, steps) => {
-  baseValues = Array.isArray(baseValues) ? baseValues : [baseValues]
-  var rangeKey = 0 - steps;
-  const newRange = {};
-  while(steps >= rangeKey) {
-    // eslint-disable-next-line no-loop-func
-    const newValues = baseValues.map((value, ind) => {
-      var newValue = value + (rangeKey * change[ind])
-      newValue = newValue > 0 ? newValue : 0;
-      newValue = newValue > 255 ? 255 : newValue
-      return newValue
-    })
-    newRange[rangeKey.toString()] = newValues.length > 1 ? newValues : newValues[0]
-    rangeKey++
-  }
-
-  return newRange
-}
-
-var createTheme = (baseValues) => {
-  const theme = {}
-  const { background, font } = baseValues
-  const baseBGColors = background.color;
-  const baseBGChange = background.change || [5, 5, 5];
-  const steps = background.range || 2;
-  theme.bgc = calcRange(baseBGColors, baseBGChange, steps)
-
-  const { fontSize, fontColor } = font;
-
-  const fontColors = fontColor.value;
-  const fontCChange = fontColor.change || [5, 5, 5];
-  const fontCRange = fontColor.range || 2;
-  theme.fc = calcRange(fontColors, fontCChange, fontCRange)
-
-  const fontSizeBase = fontSize.value;
-  const fontSChange = fontSize.change || [0.3];
-  const fontSRange = fontSize.range || 2;
-  theme.fs = calcRange(fontSizeBase, fontSChange, fontSRange)
-  return theme
-}
-
-
-var darkBase = {
-  background: {
-    color: [30,30,30],
-    change: [-5, -5, -5],
-    range: 3,
-  },
-  font: {
-    fontSize: {
-      value: 1.1,
-      range: 3,
-      change: [.1]
-    },
-    fontColor: {
-      value: [240, 240, 240]
-    }
-  }
-}
-
-
-var ligthBase = {
-  background: {
-    color: [240,240,240],
-    range: 3,
-  },
-  font: {
-    fontSize: {
-      value: 1,
-      range: 3,
-      change: [.1]
-    },
-    fontColor: {
-      value: [25, 25, 25]
-    }
-  }
-}
-
-export const darkTheme = createTheme(darkBase)
-export const lightTheme = createTheme(ligthBase)
-
 
 export default ThemeProvider
