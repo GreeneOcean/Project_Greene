1bcab7a92490173e0c4aa12ad03202a7e8971274
Elfric <elf72742@gmail.com>
2022-05-09 17:04:50 -0400

    toggle switch styling update

--- client/src/components/ToggleSwitch.js
+++ client/src/components/ToggleSwitch.js
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 const Switch = styled.label`
   position: relative;
   display: inline-block;
-  width: 60px;
-  height: 34px;
+  width: 3em;
+  height: 1.6em;
 `;
 
 const InvisibleCheckbox = styled.input`
@@ -22,16 +22,17 @@ const ToggleSwitchOff = styled.span`
   right: 0;
   bottom: 0;
   background-color: #ccc;
-  border-radius: 50%;
+  border-radius: .8em;
 
   &:before {
     position: absolute;
     content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
+    height: 1.2em;
+    width: 1.2em;
+    left: .2em;
+    bottom: .2em;
     background-color: white;
+    border-radius: 50%;
   }
 `;
 
@@ -39,9 +40,9 @@ const ToggleSwitchOn = styled(ToggleSwitchOff)`
   background-color: green;
 
   &:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
+    -webkit-transform: translateX(1.4em);
+    -ms-transform: translateX(1.4em);
+    transform: translateX(1.4em);
   }
 `;
 
@@ -49,17 +50,18 @@ const ToggleSwitch = ({on='', off='', defaultValue=false, onChange=()=>{}}) => {
   const [value, setValue] = useState(defaultValue);
 
   const toggle = (e) => {
-    //console.log(e.target.value);
     onChange(!value);
     setValue(!value);
   };
 
   return (
+    <>
     <Switch>
       <InvisibleCheckbox name="charityOnly" type="checkbox" checked={defaultValue} isChecked={value} onChange={toggle}/>
       {value ? <ToggleSwitchOn/> : <ToggleSwitchOff/>}
-      <span>{value ? on : off}</span>
     </Switch>
+    <span>{value ? on : off}</span>
+    </>
   );
 };
 
