d5ea7dde3bbfdb69c67bb652d6c3bea9def3079e
Elfric <elf72742@gmail.com>
2022-05-09 12:04:48 -0400

    moved pages dir into src dir

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import App from './app';
+import App from './src/app';
 import AppContextProvider from './appState/index.js';
 
 const Stack =
