ed6a697f7ea151450d3391938fc70aff13188a99
MarkAlperin <mark.t.alperin@gmail.com>
2022-05-10 18:46:57 -0500

    made ProductList and ProductCard for HomeItemsWidget

---
+++ client/src/components/item/ItemCard.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import { BsCardImage } from "react-icons/bs";
+
+const ItemCard = (props) => {
+  const clickHandler = () => {
+    props.setSelectedItem(props.item.id);
+  };
+
+  return (
+    <Container
+      id={props.item.id}
+      onClick={clickHandler}
+      style={{
+        backgroundColor: props.selectedItem === props.item.id ? "red" : "blue",
+      }}
+    >
+      <div style={{display: "flex", justifyContent: "space-between"}}>
+        {props.item.pictures && <img src={item.pictures[0]} />}
+        {!props.item.pictures && <BsCardImage />}
+        {props.selectedItem === props.item.id && (
+          <button
+            onClick={() => {
+              console.log("functionality not yet implemented");
+            }}
+          >
+            Add to cart
+          </button>
+        )}
+        </div>
+        <p>{props.item.title}</p>
+        <p>{props.item.description}</p>
+        {props.item.tag.map((tag, idx) => {
+          return <p key={idx}>{tag}</p>;
+        })}
+    </Container>
+  );
+};
+
+export default ItemCard;
+
+const Container = styled.div`
+  min-width: 150px;
+  max-width: 150px;
+  height: 150px;
+  margin: 10px;
+  cursor: pointer;
+`;
