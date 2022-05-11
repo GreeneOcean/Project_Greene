80999cb29236cac49ab3cc654e853975c1a155e1
MarkAlperin <mark.t.alperin@gmail.com>
2022-05-11 10:44:27 -0500

    removed code from pages/Item.js

--- client/src/pages/Item.js
+++ client/src/pages/Item.js
@@ -3,8 +3,6 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 import { PageContainer } from "../styles/index.js";
-import TextDisplay from "../components/item/TextDisplay.js";
-import Photo from "../components/item/Photo.js"
 
 function Item({ state, dispatch, init }) {
   const { dev } = state;
@@ -20,22 +18,7 @@ function Item({ state, dispatch, init }) {
     });
   }, []);
 
-  const dummyItem = {
-    username: "Mr Mark",
-    id: 1234,
-    photos: null,
-    category: "jackets",
-    tag: ["tag1", "tag2"],
-    distance: 4.5,
-    title: "what a sweet deal!",
-    description:
-      "this is a descriptions. I like tacos ANd burgers and steak and salad!",
-    rating: 5,
-  };
-  const dummyUser = {
-    lat: 30.2672,
-    lng: -97.7431,
-  };
+
 
   return (
     <PageContainer>
@@ -45,17 +28,8 @@ function Item({ state, dispatch, init }) {
       <Link to="/Donate">Donate</Link>
       <Link to="/Auth">Auth</Link>
       <Link to="/Browse">Browse</Link>
-      <StyledDiv>
-        <Photo item={dummyItem} />
-        <TextDisplay item={dummyItem} />
-      </StyledDiv>
     </PageContainer>
   );
 }
 
 export default Item;
-
-const StyledDiv = styled.div`
-  min-width: 90%;
-  max-width: 90%;
-`;
