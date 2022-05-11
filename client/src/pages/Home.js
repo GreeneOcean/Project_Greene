7bef2d1de344be235e0c9551fc16f635aa13dcc4
Ying Deng <yingdeng12345@gmail.com>
2022-05-11 18:42:10 -0500

    Yd chat merged (#12)
    * text chat working
    
    * direct message working now
    
    * text message working
    
    * after init merge, chat can not be mount on page
    
    * only one video is working
    
    * video and text chat both working
    
    * chat function working
    
    * page functioning without chat mount on DOM

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,40 +1,42 @@
-import React, { useContext, useEffect } from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { PageContainer } from '../styles/index.js';
-import Carousel from '../components/Carousel.js';
+import React, { useContext, useEffect } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { PageContainer } from "../styles/index.js";
+import Carousel from "../components/Carousel.js";
 
 function Home({ state, dispatch, init }) {
-  const { dev, user, HomeData } = state
-  const { lat, lng, local } = user
+  const { dev, user, HomeData } = state;
+  const { lat, lng, local } = user;
 
   useEffect(() => {
     init().then((res) => {
       dev.logs && console.log(`\nHome API init res`, res);
-      dev.logs && console.log('Home state', state);
+      dev.logs && console.log("Home state", state);
       dispatch({
         type: `HOME_INIT`,
-        payload: res,
+        payload: res
       });
     });
   }, []);
 
   const toggleDevLogs = (e) => {
     dispatch({
-      type: 'TOGGLE_LOGS',
+      type: "TOGGLE_LOGS"
     });
   };
 
   return (
-      <PageContainer >
-
-        <h3>Home</h3>
-        <p>{`HomeData: ${HomeData}`} </p>
-        <p>{`user lat: ${lat ? lat : 'loading'}   user lng: ${lng ? lng : 'loading'}`} </p>
-        <p>{`number of local: ${local ? local.length : 'loading'}`}</p>
-        <button onClick={toggleDevLogs} >Toggle logs</button>
-
-      </PageContainer>
+    <PageContainer>
+      <h3>Home</h3>
+      <p>{`HomeData: ${HomeData}`} </p>
+      <p>
+        {`user lat: ${lat ? lat : "loading"}   user lng: ${
+          lng ? lng : "loading"
+        }`}{" "}
+      </p>
+      <p>{`number of local: ${local ? local.length : "loading"}`}</p>
+      <button onClick={toggleDevLogs}>Toggle logs</button>
+    </PageContainer>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,118 +1,149 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser')
-
-const DB = require('./DB/index')
-const auth = require('./auth/index')
-
-const PORT = 3000;
-
-
-
+const express = require("express");
 const app = express();
-app.use(express.json());
+const http = require("http");
+const server = http.createServer(app);
+const cors = require("cors");
+const path = require("path");
+const { Server } = require("socket.io");
+const cookieParser = require("cookie-parser");
+
+const DB = require("./DB/index");
+const auth = require("./auth/index");
+
+const PORT = 8080;
+
+app.use(cors());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(auth.session);
+app.use(auth.sessionEnd);
 
-app.use( auth.session )
-app.use( auth.sessionEnd );
-
-
-app.get('/user/login', async(req, res) => {
-  try {
-    const [ userName, isAuthed ] = await auth.user(req.query)
-    if ( isAuthed ) {
-      const userData = await DB.GET.user({ userName })
-      req.user = userData
-      res.status(200).send({ user: userData })
-    }
-    else {
-      res.status(200).send({ user: null })
-    }
-
-  } catch(err) {
-    console.log('Auth user error ', err.message)
+// ===========for Chat====================================
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
   }
-})
+});
 
+io.on("connection", (socket) => {
+  console.log(`User connected 😀: ${socket.id}`);
 
-app.get('/Auth', (req, res) => {
-  res.status(200).send({ AuthData: true })
-})
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive_message", data);
+  });
 
-app.get('/Browse', (req, res) => {
-  res.status(200).send({ BrowseData: true })
-})
+  socket.on("disconnect", () => {
+    console.log("User disconnected 🥸", socket.id);
+  });
 
-app.get('/Donate', (req, res) => {
-  res.status(200).send({ DonateData: true })
-})
+  socket.on("sdp", (data) => {
+    socket.broadcast.emit("sdp", data);
+  });
 
-app.get('/Home', (req, res) => {
-  res.status(200).send({ HomeData: true })
-})
+  socket.on("candidate", (data) => {
+    socket.broadcast.emit("candidate", data);
+  });
+});
 
-app.get('/Item', (req, res) => {
-  res.status(200).send({ ItemData: true })
-})
+// Routes
+app.get("/user/login", async (req, res) => {
+  try {
+    const [userName, isAuthed] = await auth.user(req.query);
+    if (isAuthed) {
+      const userData = await DB.GET.user({ userName });
+      req.user = userData;
+      res.status(200).send({ user: userData });
+    } else {
+      res.status(200).send({ user: null });
+    }
+  } catch (err) {
+    console.log("Auth user error ", err.message);
+  }
+});
 
-app.get('/Transactions', (req, res) => {
-  res.status(200).send({ TransactionsData: true })
-})
+app.get("/Auth", (req, res) => {
+  res.status(200).send({ AuthData: true });
+});
 
+app.get("/Browse", (req, res) => {
+  res.status(200).send({ BrowseData: true });
+});
 
+app.get("/Donate", (req, res) => {
+  res.status(200).send({ DonateData: true });
+});
 
-/////////
+app.get("/Home", (req, res) => {
+  res.status(200).send({ HomeData: true });
+});
 
-app.get('/data/*', async (req, res) => {
+app.get("/Item", (req, res) => {
+  res.status(200).send({ ItemData: true });
+});
 
-  const reqPath = req.path.split('/').filter(str => !!str.length)
-  reqPath[0] = 'GET'
-  const params = req.query
+app.get("/Transactions", (req, res) => {
+  res.status(200).send({ TransactionsData: true });
+});
+
+app.get("/Item", (req, res) => {
+  console.log(`Request at ${`/Item`}`);
+  res.status(200).send({ ItemData: true });
+});
+
+app.get("/Transactions", (req, res) => {
+  console.log(`Request at ${`/Transactions`}`);
+  res.status(200).send({ TransactionsData: true });
+});
+
+app.get("/data/*", async (req, res) => {
+  const reqPath = req.path.split("/").filter((str) => !!str.length);
+  reqPath[0] = "GET";
+  const params = req.query;
 
   try {
     var query = DB;
-    reqPath.forEach(route => {
-      query = query[route]
-    })
-    const dbRes = await query(params)
-    res.status(200).send(dbRes)
-
-  } catch(err) {
-    console.log(`GET req err ${err.message} \nPATH ${req.path}`)
-    res.status(500).send({ error: true })
+    reqPath.forEach((route) => {
+      query = query[route];
+    });
+    const dbRes = await query(params);
+    res.status(200).send(dbRes);
+  } catch (err) {
+    console.log(`GET req err ${err.message} \nPATH ${req.path}`);
+    res.status(500).send({ error: true });
   }
-})
+});
 
+app.post("/AddDonation:charityID", (req, res) => {
+  console.log(`Request at ${`/AddDonation:charityID`}`);
+  res.status(201).send({ TransactionsData: true });
+});
 
-app.post('/AddDonation:charityID', (req, res) => {
-  console.log(`Request at ${`/AddDonation:charityID`}`)
-  res.status(201).send({ TransactionsData: true })
-})
+app.post("/AddReview:userID", (req, res) => {
+  console.log(`Request at ${`/AddReview:userID`}`);
+  res.status(201).send({ TransactionsData: true });
+});
 
-app.post('/AddReview:userID', (req, res) => {
-  console.log(`Request at ${`/AddReview:userID`}`)
-  res.status(201).send({ TransactionsData: true })
-})
-
-app.put('/InterestInDonation/:userID', (req, res) => {
+app.put("/InterestInDonation/:userID", (req, res) => {
   //NEED TO GET ITEM ID TOO
-  console.log(`Request at ${`/InterestInDonation/:userID`}`)
-  res.status(204).send({ TransactionsData: true })
-})
+  console.log(`Request at ${`/InterestInDonation/:userID`}`);
+  res.status(204).send({ TransactionsData: true });
+});
 
-app.put('/ApproveUserClaim/:userID', (req, res) => {
-   //NEED TO GET ITEM ID TOO
-  console.log(`Request at ${`/ApproveUserClaim/:userID`}`)
-  res.status(204).send({ TransactionsData: true })
-})
+app.put("/ApproveUserClaim/:userID", (req, res) => {
+  //NEED TO GET ITEM ID TOO
+  console.log(`Request at ${`/ApproveUserClaim/:userID`}`);
+  res.status(204).send({ TransactionsData: true });
+});
 
-app.put('/AdminApproveUser/:userID', (req, res) => {
-  console.log(`Request at ${`/AdminApproveUser/:userID`}`)
-  res.status(204).send({ TransactionsData: true })
-})
+app.put("/AdminApproveUser/:userID", (req, res) => {
+  console.log(`Request at ${`/AdminApproveUser/:userID`}`);
+  res.status(204).send({ TransactionsData: true });
+});
 
-
-app.listen(PORT, () => {
-  console.log(`Listening to port: ${PORT}`);
+// server listening
+server.listen(PORT, () => {
+  console.log(`Listening to port: ${PORT} `);
 });
