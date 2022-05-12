3d9d0e2bcd312b3743fb8c9fdb512a11dcd1eac6
MarkAlperin <mark.t.alperin@gmail.com>
2022-05-12 10:23:08 -0500

    changed map marked style to styled-components

--- client/src/components/item/ItemPageMapMarker.js
+++ client/src/components/item/ItemPageMapMarker.js
@@ -1,20 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-// import MarkerCard from "./MarkerCard.js";
 import { RiMapPin2Fill } from "react-icons/ri";
 
-const ItemPageMapMarker = ({ item, selectedItem, $hover, setSelectedItem }) => {
-  const style = { color: "blue", height: "25px", width: "25px" };
-
-
-  return (
-    <div>
-      <RiMapPin2Fill
-        style={style}
-      />
-    </div>
-  );
+const ItemPageMapMarker = ({ $hover }) => {
+  return <StyledPin />;
 };
 
 export default ItemPageMapMarker;
+
+const StyledPin = styled(RiMapPin2Fill)`
+  color: blue;
+  height: 25px;
+  width: 25px;
+`;
