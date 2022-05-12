ba1f3cb1c0c0195c11d944864b2e17ad61eb9f66
Colin Fitzhenry <cgfitzhenry@gmail.com>
2022-05-11 21:23:15 -0400

    added transactions pages (#27)
    * menu working, login / sign in working
    
    * navbar updated
    
    * created example .env.config
    
    * removed firebase from env config, still doesn't work
    
    * integrate USERNAME env variable into user init
    
    * switched username into config.js because that doesn't work either
    
    * update example env to reflect above change
    
    * fix browse.js
    
    * updated bars on top
    
    * added things and stuff
    
    * update
    
    * chat working, video not
    
    * chat app in lower right
    
    * added transactions page
    
    Co-authored-by: Elfric <elf72742@gmail.com>

--- client/src/Chat/Chat.js
+++ client/src/Chat/Chat.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import VideoPlayer from "./Dashboard/VideoPlayer";
 
 function Chat({ socket }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
 
-  const sendMessage = async () => {
+  const sendMessage = () => {
     if (currentMessage !== "") {
       const messageData = {
         message: currentMessage,
@@ -14,10 +15,11 @@ function Chat({ socket }) {
           ":" +
           new Date(Date.now()).getMinutes()
       };
-      await socket.emit("send_message", messageData);
-      setMessageList((list) => [...list, messageData]);
-      setCurrentMessage("");
-    }
+      socket.emit("send_message", messageData, (res) => {
+        setMessageList((list) => [...list, messageData]);
+        setCurrentMessage("");
+      })
+    };
   };
 
   useEffect(() => {
@@ -27,32 +29,51 @@ function Chat({ socket }) {
   }, [socket]);
 
   return (
-    <div>
-      <div className="chat-header">
-        <p>Live Chat</p>
+    <ChatContainer>
+
+      <div>
+        <div className="chat-header">
+          <p>Live Chat</p>
+        </div>
+        <div className="chat-body">
+          {messageList.map((messageContent) => {
+            return <h1>{messageContent.message}</h1>;
+          })}
+        </div>
+        <div className="chat-footer">
+          <input
+            type="text"
+            value={currentMessage}
+            placeholder="hey...."
+            onChange={(event) => {
+              setCurrentMessage(event.target.value);
+            }}
+            onKeyDown={(event) => {
+              event.key === "Enter" && sendMessage();
+            }}
+          ></input>
+          <button onClick={sendMessage}>Send</button>
+          <VideoPlayer socket={socket} />
+        </div>
       </div>
-      <div className="chat-body">
-        {messageList.map((messageContent) => {
-          return <h1>{messageContent.message}</h1>;
-        })}
-      </div>
-      <div className="chat-footer">
-        <input
-          type="text"
-          value={currentMessage}
-          placeholder="hey...."
-          onChange={(event) => {
-            setCurrentMessage(event.target.value);
-          }}
-          onKeyDown={(event) => {
-            event.key === "Enter" && sendMessage();
-          }}
-        ></input>
-        <button onClick={sendMessage}>Send</button>
-        <VideoPlayer socket={socket} />
-      </div>
-    </div>
+    </ChatContainer>
+
   );
 }
+
+const ChatContainer = styled.nav`
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  height: 60px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 1.2rem;
+  background: #37782c;
+  position: fixed;
+  bottom: 0;
+`;
 
 export default Chat;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { StateContext, DispatchContext } from '../appState';
 import styled from 'styled-components';
-import api from '../../api';
+import api from '../api';
+import { ButtonMD, LogSignButton } from '../styles/buttons';
+import { AuthInput } from '../styles/input';
 
 const Login = ({ handleClickOther }) => {
+  // const [ ,dispatch] = useContext(DispatchContext);
+  // const [state] = useContext(StateContext);
   const [userText, setUserText] = useState('');
   const [passText, setPassText] = useState('');
 
@@ -15,19 +20,18 @@ const Login = ({ handleClickOther }) => {
   };
 
   const handleLogin = () => {
-    api.get.login({
-      userId: userText,
-      attempt: passText
-    })
-    .then(res => console.log(res))
-    // .then(res => dispatch(, payload))
-    .catch(err => console.log(err))
+    api.get
+      .login({
+        userName: userText,
+        attempt: passText,
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
     <>
-      <h1>Log in</h1>
-      <input
+      <h1 style={{ marginBottom: '15px' }}>Welcome!</h1>
+      <AuthInput
         id="search"
         type="search"
         autoComplete="off"
@@ -40,7 +44,7 @@ const Login = ({ handleClickOther }) => {
           backgroundColor: 'rgb(245,245,245)',
           transition: 'all 0.5s',
           cursor: 'text',
-          marginBottom: '15px'
+          marginBottom: '15px',
         }}
         value={userText}
         onChange={handleUserChange}
@@ -59,47 +63,24 @@ const Login = ({ handleClickOther }) => {
           backgroundColor: 'rgb(245,245,245)',
           transition: 'all 0.5s',
           cursor: 'text',
-          marginBottom: '15px'
+          marginBottom: '15px',
         }}
         value={passText}
         onChange={handlePassChange}
         placeholder={'Enter your Password'}
       />
-      <ButtonMD onClick={handleLogin}>Log in</ButtonMD>
+      <ButtonMD
+        onClick={handleLogin}
+        style={{
+          marginBottom: '15px',
+        }}
+      >
+        Log in
+      </ButtonMD>
       <div>Don't have an account? </div>
-      <SignUpButton onClick={handleClickOther}>Sign Up</SignUpButton>
+      <LogSignButton onClick={handleClickOther}>Sign Up</LogSignButton>
     </>
   );
 };
-
-const SignUpButton = styled.div`
-  font-weight: bold;
-  color: #37782c;
-  width: 100px;
-  &:hover {
-    cursor: pointer;
-    color: lightgreen;
-    text-decoration: underline;
-  }
-`;
-
-const ButtonMD = styled.button`
-  border: 1px solid rgba(11, 191, 125, 0.9);
-  border-radius: 6px;
-  padding: 10px 50px;
-  width: 400px;
-  text-align: center;
-  text-decoration: none;
-  color: #37782C;
-  background-color: transparent;
-  box-shadow: 5px 5px 12px -5px rgba(0, 0, 0, 0.2);
-  transition: all 0.5s;
-  &:hover {
-    cursor: pointer;
-    color: #fff;
-    background-color: #37782C;
-    box-shadow: 0px 5px 10px rgba(46, 229, 157, 0.4);
-  }
-`;
 
 export default Login;
