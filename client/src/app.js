faa089cd0d5c022aeacd2294e367bcdce5be3058
Ian Bringe <imbringeHR@gmail.com>
2022-05-12 14:33:19 -0400

    Made sure things were all working

--- client/src/app.js
+++ client/src/app.js
@@ -24,7 +24,7 @@ function App() {
   useEffect(() => {
     api.get.location(dispatch);
     const user = { userName: 'mgee', attempt: "shalom" };
-    // api.get.login(user, dispatch); // Automatic Login user === mgee
+    api.get.login(user, dispatch); // Automatic Login user === mgee
   }, []);
 
   return (
